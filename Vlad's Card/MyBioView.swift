179c059b5f44837c70e05f666c2d92a04a59ad6b
Vlad Nikitin <nikitinv91@gmail.com>
2020-05-26 19:51:25 +0300

    First version, phone and email

---
+++ Vlad's Card/MyBioView.swift
@@ -0,0 +1,21 @@
+//
+//  MyBioView.swift
+//  Vlad's Card
+//
+//  Created by Vladislav Nikitin on 5/22/20.
+//  Copyright © 2020 Vladislav Nikitin. All rights reserved.
+//
+
+import SwiftUI
+
+struct MyBioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyBioView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyBioView()
+    }
+}
